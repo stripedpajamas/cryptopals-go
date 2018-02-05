293a84e7be18107fbf5b678da3489b42ed52c5da
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-02-05 18:55:39 -0500

    Solve challenge 51 part 1

---
+++ set7/challenge51/51.go
@@ -0,0 +1,104 @@
+package challenge51
+
+import (
+	"bytes"
+	"compress/zlib"
+	"fmt"
+
+	"github.com/stripedpajamas/cryptopals/set2/challenge9"
+
+	"github.com/stripedpajamas/cryptopals/set2/challenge10"
+
+	"github.com/stripedpajamas/cryptopals/set2/challenge11"
+	"github.com/stripedpajamas/cryptopals/set3/challenge18"
+)
+
+func CompressionOracle(pt []byte, stream bool) int {
+	// random key+IV for every call
+	iv := challenge11.GenerateRandomKey()
+	nonce := iv[:8]
+	key := challenge11.GenerateRandomKey()
+
+	// format request like this:
+	//  POST / HTTP/1.1
+	//  Host: hapless.com
+	//  Cookie: sessionid=TmV2ZXIgcmV2ZWFsIHRoZSBXdS1UYW5nIFNlY3JldCE=
+	//  Content-Length: ((len(P)))
+	//  ((P))
+
+	partialFormat := "POST / HTTP/1.1\nHost: hapless.com\nCookie: sessionid=TmV2ZXIgcmV2ZWFsIHRoZSBXdS1UYW5nIFNlY3JldCE=\n"
+	formatted := fmt.Sprintf("%sContent-Length: %d\n%s", partialFormat, len(pt), string(pt))
+
+	input := []byte(formatted)
+
+	// compress the formatted input
+	var compressed bytes.Buffer
+	w := zlib.NewWriter(&compressed)
+	w.Write(input)
+	w.Close()
+
+	var enc []byte
+
+	if stream {
+		// use a stream cipher
+		enc = challenge18.CTR(compressed.Bytes(), key, nonce)
+	} else {
+		// use a block cipher
+		plaintext := challenge9.Pad(compressed.Bytes(), 16)
+		enc = challenge10.CBCEncrypter(iv, plaintext, key)
+	}
+
+	return len(enc)
+}
+
+func GuessSessionKeyStream(verbose bool) []byte {
+	recoveredSessionID := []byte("sessionid=")
+	var currentWinner int
+	for sessionIdx := 0; currentWinner != 10; sessionIdx++ {
+		currentTarget := append(recoveredSessionID, 0)
+		currentScore := CompressionOracle(currentTarget, true)
+		currentWinner = 0
+
+		for i := 1; i < 255; i++ {
+			currentTarget = append(recoveredSessionID, byte(i))
+			currentCompression := CompressionOracle(currentTarget, true)
+			if currentCompression < currentScore {
+				currentScore = currentCompression
+				currentWinner = i
+			}
+		}
+
+		recoveredSessionID = append(recoveredSessionID, byte(currentWinner))
+		if verbose {
+			fmt.Println(string(recoveredSessionID))
+		}
+	}
+
+	return recoveredSessionID
+}
+
+func GuessSessionKeyBlock(verbose bool) []byte {
+	recoveredSessionID := []byte("sessionid=")
+	var currentWinner int
+	for sessionIdx := 0; currentWinner != 10; sessionIdx++ {
+		currentTarget := append(recoveredSessionID, 0)
+		currentScore := CompressionOracle(currentTarget, false)
+		currentWinner = 0
+
+		for i := 1; i < 255; i++ {
+			currentTarget = append(recoveredSessionID, byte(i))
+			currentCompression := CompressionOracle(currentTarget, false)
+			if currentCompression < currentScore {
+				currentScore = currentCompression
+				currentWinner = i
+			}
+		}
+
+		recoveredSessionID = append(recoveredSessionID, byte(currentWinner))
+		if verbose {
+			fmt.Println(string(recoveredSessionID))
+		}
+	}
+
+	return recoveredSessionID
+}
---
+++ set7/challenge51/51_test.go
@@ -0,0 +1,27 @@
+package challenge51
+
+import (
+	"testing"
+)
+
+func TestCompressionOracle(t *testing.T) {
+	length := CompressionOracle([]byte("hello world"), true)
+
+	if length != 133 {
+		t.Fail()
+	}
+}
+
+func TestGuessSessionKeyStream(t *testing.T) {
+	guessedSessionKey := GuessSessionKeyStream(false)
+	if string(guessedSessionKey) != "sessionid=TmV2ZXIgcmV2ZWFsIHRoZSBXdS1UYW5nIFNlY3JldCE=\n" {
+		t.Fail()
+	}
+}
+
+func TestGuessSessionKeyBlock(t *testing.T) {
+	guessedSessionKey := GuessSessionKeyBlock(true)
+	if string(guessedSessionKey) != "sessionid=TmV2ZXIgcmV2ZWFsIHRoZSBXdS1UYW5nIFNlY3JldCE=\n" {
+		t.Fail()
+	}
+}
