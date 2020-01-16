16a71247fa92ebb9a0e2b97c44c44e11def48a67
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-01-16 09:55:29 -0800

    Implement finding coprime factors of DH params

--- set8/challenge57/57.go
+++ set8/challenge57/57.go
@@ -1,1 +1,66 @@
 package challenge57
+
+import (
+	"fmt"
+	"math/big"
+)
+
+// DiscoverSecretKey attempts to recover the secret key of the other party
+// in a Diffie-Hellman key exchange
+func DiscoverSecretKey(p, g, q *big.Int, getBobMessage func(*big.Int) (string, []byte)) *big.Int {
+	return new(big.Int)
+}
+
+// GetFactors takes as input `q`, where `g^q = 1 mod p`, and
+// finds factors [r1, r2, ..., rn] of `j = (p-1) / q` until the
+// factors' product exceeds `q`
+func GetFactors(p, q *big.Int) []*big.Int {
+	one := big.NewInt(1)
+	pMinus1 := new(big.Int).Sub(p, one)
+	j := new(big.Int).Quo(pMinus1, q)
+
+	factors := make([]*big.Int, 0)
+
+	current := big.NewInt(2)
+	for !haveEnoughFactors(factors, q) {
+		if _, rem := new(big.Int).QuoRem(j, current, new(big.Int)); rem.Sign() == 0 {
+			// we have a factor, but we want to make sure it's not a repeated factor
+			if isNotRepeatedFactor(current, factors) {
+				factors = append(factors, current)
+			}
+		}
+		current = new(big.Int).Add(current, one)
+	}
+
+	return factors
+}
+
+func printFactors(factors []*big.Int) {
+	for _, f := range factors {
+		fmt.Println(f.String())
+	}
+}
+
+// multiply all the factors together and see if they're > target size
+func haveEnoughFactors(factors []*big.Int, target *big.Int) bool {
+	product := big.NewInt(1)
+	for _, factor := range factors {
+		product.Mul(product, factor)
+		if product.Cmp(target) > 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func isNotRepeatedFactor(factor *big.Int, factors []*big.Int) bool {
+	printFactors(factors)
+	one := big.NewInt(1)
+	for _, f := range factors {
+		if gcd := new(big.Int).GCD(nil, nil, f, factor); gcd.Cmp(one) != 0 {
+			// `factor` is not coprime to an already seen factor
+			return false
+		}
+	}
+	return true
+}
--- set8/challenge57/57_test.go
+++ set8/challenge57/57_test.go
@@ -7,15 +7,75 @@ import (
 	"testing"
 )
 
-func TestContrivedDH(t *testing.T) {
+func getChallengeParams() (*big.Int, *big.Int, *big.Int) {
 	p, ok := new(big.Int).SetString("7199773997391911030609999317773941274322764333428698921736339643928346453700085358802973900485592910475480089726140708102474957429903531369589969318716771", 10)
 	if !ok {
-		t.Fatal("could not create p")
+		panic("could not create p")
 	}
 	g, ok := new(big.Int).SetString("4565356397095740655436854503483826832136106141639563487732438195343690437606117828318042418238184896212352329118608100083187535033402010599512641674644143", 10)
 	if !ok {
-		t.Fatal("could not create g")
+		panic("could not create g")
 	}
+	q, ok := new(big.Int).SetString("236234353446506858198510045061214171961", 10)
+	if !ok {
+		panic("could not create q")
+	}
+
+	return p, g, q
+}
+
+func TestGetFactors(t *testing.T) {
+	p := big.NewInt(41761)
+	q := big.NewInt(144)
+	factors := GetFactors(p, q)
+
+	// coprime factors of (41761 - 1) / 144 = 290 up until their product > 144
+	expected := []*big.Int{big.NewInt(2), big.NewInt(5), big.NewInt(29)}
+
+	if len(factors) != len(expected) {
+		t.Fatalf("list of factors has different length than expected; wanted %d, got %d", len(expected), len(factors))
+	}
+	for idx := range factors {
+		if expected[idx].Cmp(factors[idx]) != 0 {
+			t.Errorf("factors not equal; wanted %s, got %s", expected[idx].String(), factors[idx].String())
+		}
+	}
+}
+
+func TestDiscoverSecretKey(t *testing.T) {
+	t.SkipNow()
+	// modeling this attack as a malicious client (Eve) repeatedly
+	// accessing a server (Bob)
+
+	p, g, q := getChallengeParams()
+	bob := NewDH(p, g)
+	if _, err := bob.Init(); err != nil {
+		t.Fatal("failed to initialize for bob")
+	}
+
+	// getBobMessage takes your public key, has bob compute his secret
+	// and the shared secret, and then uses the shared secret to hmac a
+	// message; the message and tag are returned to the client for validation
+	var getBobMessage func(*big.Int) (string, []byte)
+
+	getBobMessage = func(yourPublicKey *big.Int) (string, []byte) {
+		shared := bob.ComputeSharedSecret(yourPublicKey)
+		msg := "crazy flamboyant for the rap enjoyment"
+		h := hmac.New(sha256.New, shared.Bytes())
+		h.Write([]byte(msg))
+		mac := h.Sum(nil)
+		return msg, mac
+	}
+
+	bobSecretKey := DiscoverSecretKey(p, g, q, getBobMessage)
+
+	if bob.secret.Cmp(bobSecretKey) != 0 {
+		t.Errorf("failed to discover secret key; wanted %s, got %s", bob.secret.String(), bobSecretKey.String())
+	}
+}
+
+func TestContrivedDH(t *testing.T) {
+	p, g, _ := getChallengeParams()
 	alice := NewDH(p, g)
 	bob := NewDH(p, g)
 
